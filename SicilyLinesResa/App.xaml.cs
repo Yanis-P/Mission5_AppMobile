64f33b0badb14a40e8d5051bb669c9477bdc420c
palatete <yanisprime@hotmail.fr>
2026-03-24 08:27:09 +0100

    Initial commit

---
+++ SicilyLinesResa/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SicilyLinesResa
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
